bbc0f88158bd665da4b5d448e64a92666a1b44e5
luiscoro <luiscoronel_97@hotmail.com>
2020-07-11 16:33:42 -0500

    created

---
+++ cdolmedofe/src/main/java/net/t6/cdolmedofe/CdolmedofeApplication.java
@@ -0,0 +1,13 @@
+package net.t6.cdolmedofe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdolmedofeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdolmedofeApplication.class, args);
+	}
+
+}
---
+++ cdolmedofe/src/test/java/net/t6/cdolmedofe/CdolmedofeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.t6.cdolmedofe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdolmedofeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
